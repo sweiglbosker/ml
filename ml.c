be67114d5c7e8beeab87dede5e68c43953b085eb
stefan <stefan@s00.xyz>
2023-02-07 13:27:50 -0500

    added cost function

--- ml.c
+++ ml.c
@@ -16,8 +16,14 @@ inline double evalute(int n, double *x, double *w, double b)
    return (dot_product(n, x, w) + b); 
 }
 
-double cost(size_t n, size_t m, double *x, double *w, double b, double *y)
+double cost(size_t n, size_t m, double **x, double *w, double b, double *y)
 {
+    double cost = 0.0;
+    for (int i = 0; i < m; i++) {
+        double d = evalute(n, x[i], w, b) - y[i];
+        cost += d * d;
+    }
+    return (1.0/(2.0 * m)) * cost; 
 }
 
 struct Model *gradient_descent(size_t n, size_t m, double *x, double *y, double w_initial,
--- ml.h
+++ ml.h
@@ -12,7 +12,7 @@ struct Model *create_model(size_t nfeatures);
 /* evalute a linear function with weights w and bias b */ 
 inline double evalute(int n, double *x, double *w, double b);
 
-double cost(size_t n, size_t m, double *x, double *w, double b, double *y);
+double cost(size_t n, size_t m, double **x, double *w, double b, double *y);
 
 struct Model *gradient_descent(size_t n, size_t m, double *x, double *y, double w_initial,
-        double b_initial, double learning_rate, size_t max_iterations)
+        double b_initial, double learning_rate, size_t max_iterations);
